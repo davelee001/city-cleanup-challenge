3dda5725b7ee9657a20a79dfa999c0ff65685c78
davelee001 <david.leekaleer@student.utamu.ac.ug>
2026-07-27 09:22:22 +0200

    chore: update backend/src/config.js

--- backend/src/config.js
+++ backend/src/config.js
@@ -23,7 +23,13 @@ const config = {
   // CORS
   cors: {
     origin: process.env.CORS_ORIGIN?.split(',')
-      || ['http://localhost', 'http://localhost:19006'],
+      || [
+        'http://localhost',
+        'http://localhost:8081',
+        'http://localhost:19006',
+        'http://127.0.0.1:8081',
+        'http://127.0.0.1:19006',
+      ],
   },
 
   // API
